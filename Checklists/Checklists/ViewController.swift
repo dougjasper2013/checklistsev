a302d54db46a15fb8ca6e746f65e977f63f51865
Douglas Jasper <doug.jasper@trios.com>
2024-05-29 18:20:20 -0400

    Intitial App

---
+++ Checklists/Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Douglas Jasper on 2024-05-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
