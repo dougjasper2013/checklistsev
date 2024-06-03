06a13260179999a5d339935570d30cb187050641
Douglas Jasper <doug.jasper@trios.com>
2024-06-03 19:07:54 -0400

    End of Chapter 10

---
+++ Checklists/Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Douglas Jasper on 2024-06-03.
+//
+
+import Foundation
+
+class ChecklistItem {
+  var text = ""
+  var checked = false
+}
